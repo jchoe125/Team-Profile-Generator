1b1b67972f0f448d72b5c4b06c5f21a7df9ff97e
Joe Choe <joechoe1989@gmail.com>
2022-04-23 20:26:31 -0700

    updated index.js file with variables, reformatted folders

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Employee = require('./sheet/lib/Employee')
+const Engineer = require('./sheet/lib/Engineer');
+const Intern = require('./sheet/lib/Intern');
+const Manager = require('./sheet/lib/Manager');
