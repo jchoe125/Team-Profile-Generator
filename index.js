ff7d56afc2b84062ad9f13706bbde9c2246d5a25
Joe Choe <joechoe1989@gmail.com>
2022-04-26 12:01:13 -0700

    updated index.js file, updated readme file, added screenshots/video of application

--- index.js
+++ index.js
@@ -9,53 +9,50 @@ const generateTeam = require('./sheet/src/generateHtml');
 
 teamArray = [];
 
-// //function to initialize applications
-// function init() {
-//     runHtml();
-//     addTeam();
-// }
-
-//function to prompt user to add team members and their info
+//using inquirer to prompt user to add manager
 const addManager = () => {
     inquirer
         .prompt([{
                 type: 'input',
                 name: 'name',
-                message: "Please enter Manager's name.",
+                message: "Please enter Manager's name:",
             },
             {
                 type: 'input',
                 name: 'id',
-                message: "Please enter Manager's employee ID",
+                message: "Please enter Manager's employee ID::",
             },
             {
                 type: 'input',
                 name: 'email',
-                message: "Please enter Manager's email address",
-            } {
+                message: "Please enter Manager's email address:",
+            },
+            {
                 type: 'input',
                 name: 'officeNumber',
-                message: "Please enter Manager's office number",
+                message: "Please enter Manager's office number:",
             }
         ]).then(({ name, id, email, officeNumber }) => {
             const newManager = new Manager(name, id, email, officeNumber)
             teamArray.push(newManager)
+            addTeam()
         })
 }
 
+//using inquirer to prompt user options to add engineer, intern, or generate HTML
 const addTeam = () => {
     inquirer
         .prompt([{
             type: 'list',
-            name: 'Please select employee to add next',
-            message: 'selectEmployee',
-            choices: ['engineer', 'intern', 'exit']
+            name: 'selectEmployee',
+            message: 'Please select next type of employee to add',
+            choices: ['Engineer', 'Intern', 'Finished, exit application']
         }]).then(answers => {
             switch (answers.selectEmployee) {
-                case 'engineer':
+                case 'Engineer':
                     addEngineer()
                     break;
-                case 'intern':
+                case 'Intern':
                     addIntern()
                     break;
                 default:
@@ -64,33 +61,64 @@ const addTeam = () => {
         })
 }
 
+//using inquirer to prompt user to add engineer
 const addEngineer = () => {
-    .prompt([{
-            type: 'input',
-            name: 'name',
-            message: "Please enter Manager's name.",
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: "Please enter Manager's employee ID",
-        },
-    ]).then(({ name, id, email, github }) => {
-        const newEngineer = new Engineer(name, id, email, github) //create a new constructor with new values. 
-        team.push(newEngineer)
-        createTeam()
-    })
+    inquirer
+        .prompt([{
+                type: 'input',
+                name: 'name',
+                message: "Please enter Engineer's name:",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Please enter Engineer's employee ID:",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Please enter Engineer's email address",
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "Please enter Engineer's GitHub username:",
+            },
+        ]).then(({ name, id, email, github }) => {
+            const newEngineer = new Engineer(name, id, email, github)
+            teamArray.push(newEngineer)
+            addTeam()
+        })
 }
 
+//using inquirer to prompt user to add intern
 const addIntern = () => {
+    inquirer
         .prompt([{
-                        type: 'input',
-                        name: 'name',
-                        message: "Please enter Manager's name.",
-                    },
-                    {
-                        type: 'input',
-                        name: 'id',
-                        message: "Please enter Manager's employee ID",
-                    },
-                }
+                type: 'input',
+                name: 'name',
+                message: "Please enter Intern's name:",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "Please enter Intern's employee ID:",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Please enter Intern's email address:",
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "Please enter Intern's school name:",
+            },
+        ]).then(({ name, id, email, school }) => {
+            const newIntern = new Intern(name, id, email, school)
+            teamArray.push(newIntern)
+            addTeam()
+        })
+}
+
+addManager();
